fb020d7c7807baff3dcd4af5649470b31a4a9a52
Sean Miller <Sean.Miller@maxgaming.com.au>
2016-07-19 13:46:02 +1000

    Remove path validations. The Docker API already does an acceptable job of handling invalid paths.

--- server.js
+++ server.js
@@ -3,31 +3,18 @@
 const http = require('http');
 const docker = require('request');
 
-const validPaths = /^\/(_ping|containers|events|exec|images|info|networks|nodes|services|tasks|version|volumes)([\/\?]|$)/i;
-
-function endWithStatus(res, code) {
-  res.writeHead(code, http.STATUS_CODES[code]);
-  res.end(`${code} ${http.STATUS_CODES[code]}\r\n\r\n`);
-}
-
-// -------------------------------------------------------------------------------------------------
-// Initialise the HTTP server
-// -------------------------------------------------------------------------------------------------
-
 http.createServer((req, res) => {
   // Write the request to the console which will end up in the Docker logs for this container.
   // The Docker logs are timestamped, so won't bother to add a timestamp here.
   console.log(`${req.socket.address().address} | ${req.method} | ${req.url}`);
 
   if (req.method === 'GET') {
-    if (req.url.match(validPaths)) {
-      docker.get(`http://unix/var/run/docker.sock:${req.url}`)
-      .pipe(res);
-    } else {
-      endWithStatus(res, 404);
-    }
+    docker.get(`http://unix/var/run/docker.sock:${req.url}`)
+    .pipe(res);
   } else {
-    endWithStatus(res, 401);
+    const code = 401;
+    res.writeHead(code, http.STATUS_CODES[code]);
+    res.end(`${code} ${http.STATUS_CODES[code]}\r\n\r\n`);
   }
 })
 .listen(2375);
